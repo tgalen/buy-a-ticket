2656b973baa93bf98a94ba274e102510ffee2d8b
tgalen <tgalen84@gmail.com>
2022-02-22 20:19:22 -0500

    remove GA Pit, add random availability generator

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const seatContainer = document.getElementById("seat-container");
 const rows = ["A", "B", "C", "D", "E", "F", "G"];
 const seatNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+const seatAvailability = 50;
+console.log(seatAvailability);
+
 rows.forEach((row) => {
     let newRow = document.createElement("div");
     newRow.setAttribute("class", "row");
     seatNums.forEach((seat) => {
         let newSeat = document.createElement("div");
-        newSeat.setAttribute("class", "seat");
+        let seatValue = Math.random() * 100;
+        seatValue > seatAvailability ? newSeat.setAttribute("class", "available-seat") : newSeat.setAttribute("class", "unavailable-seat");
+        newSeat.innerHTML = `${row}${seat}`;
         newRow.appendChild(newSeat);
     });
     seatContainer.appendChild(newRow);
