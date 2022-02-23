42c1f7e8b08ebdc11f2ada709dd07032e62b34f5
tgalen <tgalen84@gmail.com>
2022-02-22 22:24:12 -0500

    psudo code for onclick seat selection

--- script.js
+++ script.js
@@ -7,14 +7,17 @@ let numOfTixPrompt = prompt("Please select the number of tickets to purchase.");
 let numOfTix = Number(numOfTixPrompt);
 console.log(numOfTix);
 
+let chnageSeatStatus = (divID) => {};
+
 rows.forEach((row) => {
     let newRow = document.createElement("div");
     newRow.setAttribute("class", "row");
     seatNums.forEach((seat) => {
         let newSeat = document.createElement("div");
         let seatValue = Math.random() * 100;
-        seatValue > seatAvailability ? newSeat.setAttribute("class", "available-seat") : newSeat.setAttribute("class", "unavailable-seat");
+        seatValue > seatAvailability ? newSeat.setAttribute("class", "available-seat") : newSeat.setAttribute("class", "unavailable-seat"); // add onlcick function to each button if 'available'
         newSeat.innerHTML = `${row}${seat}`;
+        newSeat.setAttribute("id", `${row}${seat}`);
         newRow.appendChild(newSeat);
     });
     seatContainer.appendChild(newRow);
