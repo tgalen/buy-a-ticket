027388332cbe9599cb54f8decaec8f8c29c519f2
tgalen <tgalen84@gmail.com>
2022-02-25 21:24:36 -0500

    remove num of deat input

--- script.js
+++ script.js
@@ -2,15 +2,7 @@ const seatContainer = document.getElementById("seat-container");
 const rows = ["A", "B", "C", "D", "E", "F", "G", "H"];
 const seatNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 const seatAvailability = 50; // approx % of seats unavailable
-
-// // let numOfTix = parseInt(document.getElementById("ticket-slider").value);
-// let tixNumDisplay = document.getElementById("num-of-tix-display");
-// tixNumDisplay.innerHTML = numOfTix;
-// let updateNumOfTix = (num) => {
-//     numOfTix = num;
-//     tixNumDisplay.innerHTML = numOfTix;
-// };
-
+const maxNumTix = 8;
 let changeSeatStatus = (divID) => {
     let clickedSeat = document.getElementById(divID);
     let seatToReserve = document.createElement("div");
@@ -27,7 +19,7 @@ let changeSeatStatus = (divID) => {
         let removeReservedSeat = document.getElementById(`reserved${divID}`);
         removeReservedSeat.parentNode.removeChild(removeReservedSeat);
     }
-    if (savedSeat.length > numOfTix) {
+    if (savedSeat.length > maxNumTix) {
         document.getElementById(`${savedSeat[0].innerHTML}`).setAttribute("class", "available-seat");
         display.removeChild(savedSeat[0]);
     }
