34e425085c163e4c8b44106e228529528a516c25
tgalen <tgalen84@gmail.com>
2022-02-26 11:01:58 -0500

    style button

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const seatContainer = document.getElementById("seat-container");
 const rows = ["A", "B", "C", "D", "E", "F", "G", "H"];
 const seatNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 const seatAvailability = 50; // approx % of seats unavailable
-const maxNumTix = 8;
+const maxNumTix = 6;
 let numOfSeatsSelected = 0;
 let confirmBtn = document.getElementById("confirm-purchase");
 if (numOfSeatsSelected < 1) {
